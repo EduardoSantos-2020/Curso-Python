4f5373b209aac1790b149305fb9c5e067fd686be
EduardoSantos-2020 <eduardosantos123444@gmail.com>
2022-01-22 19:43:39 -0300

    primeiras aulas

---
+++ aula-2.py
@@ -0,0 +1,42 @@
+
+a = int(input("Faça o primeiro valor: "))
+b = int(input("Faça o segundo valor: "))
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divicao = a / b
+resto = a % b
+print('soma: {soma} \nsubtração: {sub}'
+        '\nMultiplicação: {mult} \nDivisão: {div} '
+        '\nResto: {resto}'.format(soma=soma,
+                                 sub=subtracao,
+                                mult=multiplicacao,
+                                div=divicao,
+                                resto=resto))
+
+
+# soma = a + b
+#
+# print('soma: ' + str(soma))
+#
+# subtracao = a - b
+#
+# print(subtracao)
+#
+# multiplicacao = a * b
+#
+# print(multiplicacao)
+#
+# divisao = a / b
+#
+# print(int(divisao))
+#
+# resto = a % b
+#
+# print(resto)
+# x = '1'
+#
+# soma1 = int(x) + 1
+#
+# print(soma1)
---
+++ main.py
@@ -0,0 +1,5 @@
+
+print("Meu primeiro teste")
+a = 5
+b = 9
+print(a * b)
